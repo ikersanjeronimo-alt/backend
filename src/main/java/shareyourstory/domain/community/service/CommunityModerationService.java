887929dd200a1c81ec801c075bf40736912a8f6a
JlasaiUni <jon.lasa@alumni.mondragon.edu>
2026-06-06 00:40:40 +0200

    fix: añadir min a input de fecha en EventCreatePage

--- src/main/java/shareyourstory/domain/community/service/CommunityModerationService.java
+++ src/main/java/shareyourstory/domain/community/service/CommunityModerationService.java
@@ -1,7 +1,6 @@
 package shareyourstory.domain.community.service;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
